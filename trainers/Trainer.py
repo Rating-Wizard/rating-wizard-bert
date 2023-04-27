5295d233b4717431da7b49cbc6ed784e0d3bc9db
Hammad Ahmed <hammadahmedpk1@gmail.com>
2023-04-27 18:15:17 +0500

    Implement Train Model Function

--- trainers/Trainer.py
+++ trainers/Trainer.py
@@ -16,7 +16,31 @@ torch.manual_seed(RANDOM_SEED)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def train_model(model, data_loader, loss_fn, optimizer, device, scheduler, n_examples):
-    pass
+    model=model.train()
+    losses = []
+    correct_predictions = 0
+
+    for d in data_loader:
+        input_ids = d["input_ids"].to(device)
+        attention_mask = d["attention_mask"].to(device)
+        targets = d["targets"].to(device)
+
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+        _, preds = torch.max(outputs, dim = 1)
+        loss = loss_fn(outputs, targets)
+
+        correct_predictions += torch.sum(preds == targets).cpu()
+
+        losses.append(loss.item())
+
+        loss.backward()
+        nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        optimizer.step()
+        scheduler.step()
+        optimizer.zero_grad()
+
+    return correct_predictions/n_examples, np.mean(losses)
+''' clip_grad_norm - We tackle the problem of exploding and vanishing gradients By gradient clipping '''
 
 def get_predictions(model, data_loader):
     pass
